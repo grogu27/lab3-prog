fec897259218266f71400778837f0f80e0a20076
Soskin Andrey <arthas@-.localdomain>
2023-05-02 21:42:10 +0700

    add test case

--- first/main.c
+++ first/main.c
@@ -3,7 +3,16 @@
 
 int main(int argv, char **argc)
 {
-    
+    if (argc[1])
+    {
+        FILE *test = fopen("test.txt", "wb");
+        uint32_t code_point;
+        uint8_t buf_test[4];
+        sscanf(argc[1], "%d", &code_point);
+        int len = encode_varint(code_point, buf_test);
+        fwrite(buf_test, sizeof(uint8_t), len, test);
+        fclose(test);
+    }
     const char compress[] = "compressed.dat"; 
     const char uncompress[] = "uncompressed.dat";
 
